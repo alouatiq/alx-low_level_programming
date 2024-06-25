cf61df2abe0c4670312b7cfa113201ddd79f579a
alouatiq <alouatiq@outlook.com>
2024-06-25 14:16:18 -0700

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,19 +14,16 @@ x = 1;  /* First Fibonacci number */
 y = 2;  /* Second Fibonacci number */
 
 /* Print the first Fibonacci number */
-printf("%lu, ", x);
+printf("%lu", x);
 
 /* Print the second Fibonacci number */
-printf("%lu, ", y);
+printf(", %lu", y);
 
 /* Calculate and print the next 96 Fibonacci numbers */
 for (count = 3; count <= 98; count++)
 {
 fib = x + y;  /* Calculate next Fibonacci number */
-printf("%lu", fib);
-
-if (count != 98)  /* Print comma and space for all but the last number */
-printf(", ");
+printf(", %lu", fib);
 
 /* Update x and y for the next iteration */
 x = y;
