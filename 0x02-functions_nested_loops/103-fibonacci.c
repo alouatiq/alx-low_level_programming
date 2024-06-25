2d94cbffb436bdbc9e3df4246c4e01aee6b84334
alouatiq <alouatiq@outlook.com>
2024-06-25 14:03:48 -0700

    103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
+ * intcalculate_sum_of_even_fibonacci - Calculate sum of even Fibonacci numbers up to 4,000,000
  */
-void intcalculate_sum_of_even_fibonacci (void)
+void intcalculate_sum_of_even_fibonacci(void)
 {
-long int i, x = 1, y = 2, sum = 0, tSum = 0;
+  long int x = 1, y = 2, sum = 0, tSum = 0;
 
-// Loop to calculate the Fibonacci sequence and sum even numbers
-for (i = 0; i < 49; i++) // Iterate 49 times to find the first 50 Fibonacci numbers
-{
-if ((y % 2 == 0) && (y <= 4000000)) // Check if the current Fibonacci number (y) is even and not exceed 4,000,000
-{
-tSum = tSum + y; // Add the even Fibonacci number to the total sum (tSum)
-}
-sum = x + y; // Calculate the next Fibonacci number
-x = y; // Update the previous Fibonacci number
-y = sum; // Update the current Fibonacci number
-}
-printf("%ld\n", tSum); // Print the total sum of even Fibonacci numbers
+  /* Loop to calculate the Fibonacci sequence and sum even numbers */
+  while (y <= 4000000)
+    {
+      if (y % 2 == 0)
+	{
+	  tSum += y;
+	}
+      sum = x + y;
+      x = y;
+      y = sum;
+    }
 
-return (0); // Return 0 to indicate successful execution
+  /* Print the sum of even Fibonacci numbers */
+  printf("%ld\n", tSum);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+  intcalculate_sum_of_even_fibonacci();
+  return (0);
 }
