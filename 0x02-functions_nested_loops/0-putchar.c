e6267837574d5ebe9c91d6325c67b364bac7910e
alouatiq <alouatiq@outlook.com>
2024-06-25 10:10:50 -0700

    task 1+2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar followed by a new line
+ * Return: 0 on success
+ */
+int main(void)
+{
+  putchar('_');
+  putchar('p');
+  putchar('u');
+  putchar('t');
+  putchar('c');
+  putchar('h');
+  putchar('a');
+  putchar('r');
+  putchar('\n');
+
+  return 0;
+}
