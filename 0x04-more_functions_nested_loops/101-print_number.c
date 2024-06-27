ce1d8effbb4cbab7787efd2732ee6ef828cedbca
alouatiq <alouatiq@outlook.com>
2024-06-27 11:51:45 -0700

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,37 +6,29 @@
  */
 void print_number(int n)
 {
-int reversed = 0;
-int digit;
-int is_negative = 0;
+unsigned int m, d, count;
 
-  /* Handle negative numbers */
-if (n < 0) {
-is_negative = 1;
-n = -n; /* Make n positive */
+if (n < 0)
+{
+_putchar(45);
+m = n * -1;
+}
+else
+{
+m = n;
 }
 
-  /* Special case for 0 */
-if (n == 0) {
-_putchar('0');
-return;
+d = m;
+count = 1;
+
+while (d > 9)
+{
+d /= 10;
+count *= 10;
 }
 
-  /* Reverse the digits */
-while (n != 0) {
-digit = n % 10; /* Get the last digit */
-reversed = reversed * 10 + digit; /* Build reversed number */
-n /= 10; /* Move to the next digit */
-}
-
-  /* Print the reversed number */
-if (is_negative)
-_putchar('-'); /* Print negative sign if necessary */
-
-  /* Print each digit from reversed */
-while (reversed != 0) {
-digit = reversed % 10;
-_putchar(digit + '0'); /* Convert digit to character and print */
-reversed /= 10;
+for (; count >= 1; count /= 10)
+{
+_putchar(((m / count) % 10) + 48);
 }
 }
