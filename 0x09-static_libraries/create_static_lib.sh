9030321601f50ae8cf12f91c7f17b7b662e4a3c9
alouatiq <alouatiq@outlook.com>
2024-07-12 16:31:50 -0700

    task 2

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-gcc -c *.c
+
+# Compile all .c files into object files
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+
+# Create the static library liball.a from all object files
 ar rcs liball.a *.o
+
+# Clean up: remove all object files (*.o)
+rm *.o
