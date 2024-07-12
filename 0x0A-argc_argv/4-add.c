f45da7b5b96661146310209169f1a2729a151814
alouatiq <alouatiq@outlook.com>
2024-07-12 16:44:57 -0700

    all tasks

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always 0 if successful, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+int i, j, sum;
+
+sum = 0;
+if (argc < 2)
+{
+printf("0\n");
+return (0);
+}
+
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+
+printf("%d\n", sum);
+return (0);
+}
