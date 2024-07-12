b87c5e76e1b48007f3047503e256c0116ea23ef3
alouatiq <alouatiq@outlook.com>
2024-07-12 16:48:50 -0700

    all tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,8 @@
  * @argv: Argument vector
  * Return: Always 0
  */
-int main(int argc, char *argv[])
-{
-if (argc >= 1)
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", argv[0]);
-}
 return (0);
 }
