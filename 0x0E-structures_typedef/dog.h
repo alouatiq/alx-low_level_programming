57e545a88e0dbd3b97a2d0945260880d1829c990
alouatiq <alouatiq@outlook.com>
2024-07-22 03:36:22 -0700

    All tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,41 +6,19 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
- *
  * Description: Longer description
  */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 typedef struct dog dog_t;
 
-/**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner to initialize
- */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
-/**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog_t, or NULL on failure
- */
 dog_t *new_dog(char *name, float age, char *owner);
-
-/**
- * free_dog - frees dogs
- * @d: pointer to dog_t to free
- */
 void free_dog(dog_t *d);
-
+void print_dog(struct dog *d);
 #endif /* DOG_H */
