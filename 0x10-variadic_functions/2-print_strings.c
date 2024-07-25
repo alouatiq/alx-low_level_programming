f7e0d3c6b8dc8cc3400a4935364ab9ab9d9b1a4e
alouatiq <alouatiq@outlook.com>
2024-07-25 12:08:40 -0700

    All tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,25 +9,25 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-  va_list args;
-  unsigned int i;
-  char *str;
+va_list args;
+unsigned int i;
+char *str;
 
-  va_start(args, n);
+va_start(args, n);
 
-  for (i = 0; i < n; i++) {
-    str = va_arg(args, char *);
+for (i = 0; i < n; i++) {
+str = va_arg(args, char *);
 
-    if (str)
-      printf("%s", str);
-    else
-      printf("(nil)");
+if (str)
+printf("%s", str);
+else
+printf("(nil)");
 
-    if (separator && i < n - 1)
-      printf("%s", separator);
-  }
+if (separator && i < n - 1)
+printf("%s", separator);
+}
 
-  printf("\n");
+printf("\n");
 
-  va_end(args);
+va_end(args);
 }
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,37 +8,37 @@
  */
 void print_all(const char * const format, ...)
 {
-  va_list args;
-  unsigned int i = 0;
-  char *str, *separator = "";
+va_list args;
+unsigned int i = 0;
+char *str, *separator = "";
 
-  va_start(args, format);
+va_start(args, format);
 
-  while (format && format[i]) {
-    switch (format[i]) {
-    case 'c':
-      printf("%s%c", separator, va_arg(args, int));
-      break;
-    case 'i':
-      printf("%s%d", separator, va_arg(args, int));
-      break;
-    case 'f':
-      printf("%s%f", separator, va_arg(args, double));
-      break;
-    case 's':
-      str = va_arg(args, char *);
-      if (!str)
-	str = "(nil)";
-      printf("%s%s", separator, str);
-      break;
-    default:
-      i++;
-      continue;
-    }
-    separator = ", ";
-    i++;
-  }
+while (format && format[i]) {
+switch (format[i]) {
+case 'c':
+printf("%s%c", separator, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", separator, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", separator, va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", separator, str);
+break;
+default:
+i++;
+continue;
+}
+separator = ", ";
+i++;
+}
 
-  printf("\n");
-  va_end(args);
+printf("\n");
+va_end(args);
 }
