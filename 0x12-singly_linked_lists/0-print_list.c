e2b8ef567325935d884f68ab699f79fd8110fe40
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-01 17:30:47 +0200

    All tasks

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h)
+    {
+        if (h->str)
+            printf("[%u] %s
+", h->len, h->str);
+        else
+            printf("[0] (nil)
+");
+
+        count++;
+        h = h->next;
+    }
+
+    return (count);
+}
---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+
+section .data
+    msg db 'Hello, Holberton', 0xa
+    len equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    ; write the message to stdout
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; file descriptor (stdout)
+    mov rsi, msg        ; address of the message
+    mov rdx, len        ; length of the message
+    syscall
+
+    ; exit the program
+    mov rax, 60         ; sys_exit
+    xor rdi, rdi        ; status 0
+    syscall
