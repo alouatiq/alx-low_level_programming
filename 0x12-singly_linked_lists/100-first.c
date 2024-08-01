81c85d96b255d26ab557cc06dd7d998ee4ff2f15
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-01 17:36:45 +0200

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void __attribute__((constructor)) hare_and_tortoise(void);
  */
 void hare_and_tortoise(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
