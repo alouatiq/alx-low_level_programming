f60c8f9208f9fec3590382825c696f609d12a6c4
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-01 17:37:43 +0200

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,19 @@
-
 section .data
-    msg db 'Hello, Holberton', 0xa
-    len equ $ - msg
+msg db 'Hello, Holberton', 0xa
+len equ $ - msg
 
 section .text
-    global _start
+global _start
 
 _start:
-    ; write the message to stdout
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; file descriptor (stdout)
-    mov rsi, msg        ; address of the message
-    mov rdx, len        ; length of the message
-    syscall
+; write the message to stdout
+mov rax, 1          ; sys_write
+mov rdi, 1          ; file descriptor (stdout)
+mov rsi, msg        ; address of the message
+mov rdx, len        ; length of the message
+syscall
 
-    ; exit the program
-    mov rax, 60         ; sys_exit
-    xor rdi, rdi        ; status 0
-    syscall
+; exit the program
+mov rax, 60         ; sys_exit
+xor rdi, rdi        ; status 0
+syscall
