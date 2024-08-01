7095af0776b4d846ea026cb8b9c40823353f3050
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-01 22:25:32 +0200

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,37 +10,37 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
-    unsigned int len = 0;
+list_t *new_node, *temp;
+unsigned int len = 0;
 
-    while (str[len])
-        len++;
+while (str[len])
+len++;
 
-    new_node = malloc(sizeof(list_t));
-    if (!new_node)
-        return (NULL);
+new_node = malloc(sizeof(list_t));
+if (!new_node)
+return (NULL);
 
-    new_node->str = strdup(str);
-    if (!new_node->str)
-    {
-        free(new_node);
-        return (NULL);
-    }
+new_node->str = strdup(str);
+if (!new_node->str)
+{
+free(new_node);
+return (NULL);
+}
 
-    new_node->len = len;
-    new_node->next = NULL;
+new_node->len = len;
+new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
 
-    temp = *head;
-    while (temp->next)
-        temp = temp->next;
+temp = *head;
+while (temp->next)
+temp = temp->next;
 
-    temp->next = new_node;
+temp->next = new_node;
 
-    return (new_node);
+return (new_node);
 }
