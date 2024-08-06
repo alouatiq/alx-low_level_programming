821552b02816acd985626dc79999df651ef850e6
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-06 16:31:37 +0200

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,25 +8,25 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-    size_t count = 0;
-    listint_t *current, *next;
+size_t count = 0;
+listint_t *current, *next;
 
-    if (!h || !*h)
-        return 0;
+if (!h || !*h)
+return 0;
 
-    current = *h;
-    while (current)
-    {
-        count++;
-        next = current->next;
-        free(current);
+current = *h;
+while (current)
+{
+count++;
+next = current->next;
+free(current);
 
-        if (next == *h)
-            break;
+if (next == *h)
+break;
 
-        current = next;
-    }
+current = next;
+}
 
-    *h = NULL;
-    return count;
+*h = NULL;
+return count;
 }
