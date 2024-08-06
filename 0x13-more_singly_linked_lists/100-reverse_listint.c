7330b380f7ae996b05a7d4b0e99b9b0fe8a44099
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-06 16:30:30 +0200

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,16 +7,16 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *prev = NULL, *next = NULL;
+listint_t *prev = NULL, *next = NULL;
 
-    while (*head)
-    {
-        next = (*head)->next;
-        (*head)->next = prev;
-        prev = *head;
-        *head = next;
-    }
+while (*head)
+{
+next = (*head)->next;
+(*head)->next = prev;
+prev = *head;
+*head = next;
+}
 
-    *head = prev;
-    return *head;
+*head = prev;
+return *head;
 }
