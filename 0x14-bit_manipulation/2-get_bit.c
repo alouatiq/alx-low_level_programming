72b802c3c7999f008ef58d3ca2323a51d522b618
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-08 11:50:20 +0200

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,13 +4,12 @@
  * get_bit - returns the value of a bit at a given index.
  * @n: number to search
  * @index: index of the bit
- *
  * Return: the value of the bit at index or -1 if an error occurred
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index > (sizeof(unsigned long int) * 8 - 1))
-        return (-1);
+if (index > (sizeof(unsigned long int) * 8 - 1))
+return (-1);
 
-    return ((n >> index) & 1);
+return ((n >> index) & 1);
 }
