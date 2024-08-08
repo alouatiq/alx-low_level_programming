9917ce0c32ee8679ffbdea2c7516207b17aa8dbb
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-08 11:51:22 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,19 +5,18 @@
  * to another.
  * @n: first number
  * @m: second number
- *
  * Return: the number of bits to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int xor_result = n ^ m;
-    unsigned int count = 0;
+unsigned long int xor_result = n ^ m;
+unsigned int count = 0;
 
-    while (xor_result)
-    {
-        count += xor_result & 1;
-        xor_result >>= 1;
-    }
+while (xor_result)
+{
+count += xor_result & 1;
+xor_result >>= 1;
+}
 
-    return (count);
+return (count);
 }
