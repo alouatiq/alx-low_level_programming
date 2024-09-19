ec2c1a57bb828614ff3392d8356576903218f5c6
AL OUATIQ H <alouatiq@outlook.com>
2024-09-19 01:05:06 -0700

    fixing betty

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,24 +3,24 @@
 #include <string.h>
 
 char* generate_key(char* username) {
-    // This function should implement the key generation algorithm
-    // based on the analysis of crackme5
-    // For now, it's just a placeholder
-    char* key = malloc(strlen(username) + 1);
-    strcpy(key, username);
-    // Modify key based on the crackme5 algorithm
-    return key;
+	// This function should implement the key generation algorithm
+	// based on the analysis of crackme5
+	// For now, it's just a placeholder
+	char* key = malloc(strlen(username) + 1);
+	strcpy(key, username);
+	// Modify key based on the crackme5 algorithm
+	return key;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s username\n", argv[0]);
-        return 1;
-    }
+	if (argc != 2) {
+	fprintf(stderr, "Usage: %s username\n", argv[0]);
+	return 1;
+	}
 
-    char* key = generate_key(argv[1]);
-    printf("%s\n", key);
-    free(key);
+	char* key = generate_key(argv[1]);
+	printf("%s\n", key);
+	free(key);
 
-    return 0;
+	return 0;
 }
--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,17 +2,17 @@
 #include "lists.h"
 
 /**
- * free_dlistint - Frees a dlistint_t list
- * @head: Pointer to the head of the list
- */
+	* free_dlistint - Frees a dlistint_t list
+	* @head: Pointer to the head of the list
+	*/
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
+	dlistint_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
+	while (head != NULL)
+	{
+	temp = head;
+	head = head->next;
+	free(temp);
+	}
 }
