a46700d2764ab6e1c02bac1b0cef35620d93203a
AL OUATIQ H <alouatiq@outlook.com>
2024-09-19 13:23:01 -0700

    crackme4

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -4,31 +4,31 @@
 
 char *generate_key(char *username)
 {
-    static char key[7];
-    int username_len;
+	static char key[7];
+	int username_len;
 
-    username_len = strlen(username);
+	username_len = strlen(username);
 
-    /* This is a placeholder algorithm. Replace with the actual algorithm after analyzing crackme5 */
-    key[0] = username[0] + username_len;
-    key[1] = 'A' + (username_len % 26);
-    key[2] = '0' + (username_len % 10);
-    key[3] = username[username_len - 1] - 2;
-    key[4] = '@' + username_len;
-    key[5] = username[0] - username_len;
-    key[6] = '\0';
+	/* This is a placeholder algorithm. Replace with the actual algorithm after analyzing crackme5 */
+	key[0] = username[0] + username_len;
+	key[1] = 'A' + (username_len % 26);
+	key[2] = '0' + (username_len % 10);
+	key[3] = username[username_len - 1] - 2;
+	key[4] = '@' + username_len;
+	key[5] = username[0] - username_len;
+	key[6] = '\0';
 
-    return (key);
+	return (key);
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s username\n", argv[0]);
-        return (1);
-    }
+	if (argc != 2)
+	{
+	fprintf(stderr, "Usage: %s username\n", argv[0]);
+	return (1);
+	}
 
-    printf("%s\n", generate_key(argv[1]));
-    return (0);
+	printf("%s\n", generate_key(argv[1]));
+	return (0);
 }
