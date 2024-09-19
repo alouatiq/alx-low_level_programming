7c4f1f185db0f8f7cd6b41646aedc65621d0424b
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-19 21:53:19 +0200

    Create palindrome_finder.c

---
+++ 0x17-doubly_linked_lists/palindrome_finder.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int is_palindrome(int n) {
+    int reversed = 0, original = n;
+    while (n != 0) {
+        reversed = reversed * 10 + n % 10;
+        n /= 10;
+    }
+    return original == reversed;
+}
+
+int main() {
+    int largest_palindrome = 0;
+    int i, j;
+
+    for (i = 999; i >= 100; i--) {
+        for (j = i; j >= 100; j--) {
+            int product = i * j;
+            if (product > largest_palindrome && is_palindrome(product)) {
+                largest_palindrome = product;
+            }
+        }
+    }
+
+    printf("%d\n", largest_palindrome);
+
+    FILE *file = fopen("102-result", "w");
+    if (file != NULL) {
+        fprintf(file, "%d", largest_palindrome);
+        fclose(file);
+    }
+
+    return 0;
+}
