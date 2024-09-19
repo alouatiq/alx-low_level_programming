c22f10eb968568d738b67515806b64869bab24d6
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-19 22:29:33 +0200

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -2,32 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * generate_key - Generates a key based on the username
+ * @username: The input username
+ *
+ * Return: A pointer to the generated key
+ */
 char *generate_key(char *username)
 {
 	static char key[7];
-	int username_len;
+	int i, username_len;
+	unsigned long sum = 0;
 
 	username_len = strlen(username);
 
-	/* This is a placeholder algorithm. Replace with the
- 	* actual algorithm after analyzing crackme5 */
-	key[0] = username[0] + username_len;
-	key[1] = 'A' + (username_len % 26);
-	key[2] = '0' + (username_len % 10);
-	key[3] = username[username_len - 1] - 2;
-	key[4] = '@' + username_len;
-	key[5] = username[0] - username_len;
+	for (i = 0; i < username_len; i++)
+	{
+		sum += username[i];
+	}
+
+	srand(sum ^ 0x1234);
+
+	for (i = 0; i < 6; i++)
+	{
+		key[i] = rand() % 26 + 'A';
+	}
 	key[6] = '\0';
 
 	return (key);
 }
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-	fprintf(stderr, "Usage: %s username\n", argv[0]);
-	return (1);
+		fprintf(stderr, "Usage: %s username\n", argv[0]);
+		return (1);
 	}
 
 	printf("%s\n", generate_key(argv[1]));
