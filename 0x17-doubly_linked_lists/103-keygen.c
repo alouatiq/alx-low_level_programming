4d60e5c39be71569b837d6c45946e4ec8a4ca244
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-09-19 21:59:03 +0200

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -4,35 +4,31 @@
 
 char *generate_key(char *username)
 {
-    char *key;
-    /* Your key generation algorithm here */
-    key = malloc(strlen(username) + 1);
-    if (key == NULL)
-        return NULL;
-    strcpy(key, username);
-    /* Modify key based on the crackme5 algorithm */
+    static char key[7];
+    int i, username_len;
+
+    username_len = strlen(username);
+
+    // This is a placeholder algorithm. Replace with the actual algorithm after analyzing crackme5
+    key[0] = username[0] + username_len;
+    key[1] = 'A' + (username_len % 26);
+    key[2] = '0' + (username_len % 10);
+    key[3] = username[username_len - 1] - 2;
+    key[4] = '@' + username_len;
+    key[5] = username[0] - username_len;
+    key[6] = '\0';
+
     return key;
 }
 
 int main(int argc, char *argv[])
 {
-    char *key;
-
     if (argc != 2)
     {
         fprintf(stderr, "Usage: %s username\n", argv[0]);
         return 1;
     }
 
-    key = generate_key(argv[1]);
-    if (key == NULL)
-    {
-        fprintf(stderr, "Failed to generate key\n");
-        return 1;
-    }
-
-    printf("%s\n", key);
-    free(key);
-
+    printf("%s\n", generate_key(argv[1]));
     return 0;
 }
