9a731a3116676d971f57cf1803b6fbd7800a2217
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-22 10:45:40 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,66 +5,65 @@
 #include <elf.h>
 
 /**
- * print_error - Prints an error message to stderr and exits.
+ * print_error - Prints an error message to stderr and exits with status 98.
  * @msg: The error message to print.
  */
 void print_error(const char *msg)
 {
-    dprintf(STDERR_FILENO, "%s\n", msg);
-    exit(98);
+	dprintf(STDERR_FILENO, "%s\n", msg);
+	exit(98);
 }
 
 /**
- * print_elf_header - Prints the ELF header information.
+ * print_elf_header - Prints the ELF header information in the correct format.
  * @header: Pointer to the ELF header structure.
  */
 void print_elf_header(const Elf64_Ehdr *header)
 {
-    int i;
+	int i;
 
-    printf("ELF Header:\n");
+	printf("ELF Header:\n");
 
-    /* Magic */
-    printf("  Magic:   ");
-    for (i = 0; i < EI_NIDENT; i++)
-        printf("%02x%c", header->e_ident[i], i == EI_NIDENT - 1 ? '\n' : ' ');
+	/* Magic */
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x%c", header->e_ident[i], i == EI_NIDENT - 1 ? '\n' : ' ');
 
-    /* Class */
-    printf("  Class:                             %s\n",
-           header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
-           header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "Invalid class");
+	/* Class */
+	printf("  Class:                             %s\n",
+		header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
+		header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "Invalid class");
 
-    /* Data */
-    printf("  Data:                              %s\n",
-           header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
-           header->e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "Invalid data encoding");
+	/* Data */
+	printf("  Data:                              %s\n",
+		header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
+		header->e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "Invalid data encoding");
 
-    /* Version */
-    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+	/* Version */
+	printf("  Version:                           %d\n", header->e_ident[EI_VERSION]);
 
-    /* OS/ABI */
-    printf("  OS/ABI:                            %s\n",
-           header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
-           header->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "UNIX - NetBSD" :
-           header->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ? "UNIX - Solaris" :
-           "<unknown>");
+	/* OS/ABI */
+	printf("  OS/ABI:                            %s\n",
+		header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
+		header->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "UNIX - NetBSD" :
+		header->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ? "UNIX - Solaris" :
+		"<unknown>");
 
-    /* ABI Version */
-    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	/* ABI Version */
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
 
-    /* Type */
-    printf("  Type:                              %s\n",
-           header->e_type == ET_EXEC ? "EXEC (Executable file)" :
-           header->e_type == ET_DYN ? "DYN (Shared object file)" :
-           header->e_type == ET_REL ? "REL (Relocatable file)" : "Unknown");
+	/* Type */
+	printf("  Type:                              %s\n",
+		header->e_type == ET_EXEC ? "EXEC (Executable file)" :
+		header->e_type == ET_DYN ? "DYN (Shared object file)" :
+		header->e_type == ET_REL ? "REL (Relocatable file)" : "Unknown");
 
-    /* Entry point address */
-    printf("  Entry point address:               0x%lx\n", (unsigned long)header->e_entry);
+	/* Entry point address */
+	printf("  Entry point address:               0x%lx\n", (unsigned long)header->e_entry);
 }
 
 /**
- * main - Displays the information contained in the ELF header at the start
- *        of an ELF file.
+ * main - Displays the information contained in the ELF header of an ELF file.
  * @argc: The number of arguments.
  * @argv: An array of arguments.
  *
@@ -72,35 +71,35 @@ void print_elf_header(const Elf64_Ehdr *header)
  */
 int main(int argc, char *argv[])
 {
-    int fd;
-    ssize_t read_bytes;
-    Elf64_Ehdr header;
+	int fd;
+	ssize_t read_bytes;
+	Elf64_Ehdr header;
 
-    if (argc != 2)
-        print_error("Usage: elf_header elf_filename");
+	if (argc != 2)
+		print_error("Usage: elf_header elf_filename");
 
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-        print_error("Error: Can't read file");
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		print_error("Error: Can't read file");
 
-    read_bytes = read(fd, &header, sizeof(header));
-    if (read_bytes == -1 || read_bytes != sizeof(header))
-    {
-        close(fd);
-        print_error("Error: Can't read file");
-    }
+	read_bytes = read(fd, &header, sizeof(header));
+	if (read_bytes == -1 || read_bytes != sizeof(header))
+	{
+		close(fd);
+		print_error("Error: Can't read file");
+	}
 
-    if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-        header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 ||
-        header.e_ident[EI_MAG3] != ELFMAG3)
-    {
-        close(fd);
-        print_error("Error: Not an ELF file");
-    }
+	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+		header.e_ident[EI_MAG1] != ELFMAG1 ||
+		header.e_ident[EI_MAG2] != ELFMAG2 ||
+		header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		close(fd);
+		print_error("Error: Not an ELF file");
+	}
 
-    print_elf_header(&header);
+	print_elf_header(&header);
 
-    close(fd);
-    return (0);
+	close(fd);
+	return (0);
 }
