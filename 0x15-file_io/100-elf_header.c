e5eee479427f8e18b688e1edaa44b5f6bb401c56
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-22 10:42:21 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -15,6 +15,54 @@ void print_error(const char *msg)
 }
 
 /**
+ * print_elf_header - Prints the ELF header information.
+ * @header: Pointer to the ELF header structure.
+ */
+void print_elf_header(const Elf64_Ehdr *header)
+{
+    int i;
+
+    printf("ELF Header:\n");
+
+    /* Magic */
+    printf("  Magic:   ");
+    for (i = 0; i < EI_NIDENT; i++)
+        printf("%02x%c", header->e_ident[i], i == EI_NIDENT - 1 ? '\n' : ' ');
+
+    /* Class */
+    printf("  Class:                             %s\n",
+           header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
+           header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "Invalid class");
+
+    /* Data */
+    printf("  Data:                              %s\n",
+           header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
+           header->e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "Invalid data encoding");
+
+    /* Version */
+    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+
+    /* OS/ABI */
+    printf("  OS/ABI:                            %s\n",
+           header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
+           header->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "UNIX - NetBSD" :
+           header->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ? "UNIX - Solaris" :
+           "<unknown>");
+
+    /* ABI Version */
+    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+
+    /* Type */
+    printf("  Type:                              %s\n",
+           header->e_type == ET_EXEC ? "EXEC (Executable file)" :
+           header->e_type == ET_DYN ? "DYN (Shared object file)" :
+           header->e_type == ET_REL ? "REL (Relocatable file)" : "Unknown");
+
+    /* Entry point address */
+    printf("  Entry point address:               0x%lx\n", (unsigned long)header->e_entry);
+}
+
+/**
  * main - Displays the information contained in the ELF header at the start
  *        of an ELF file.
  * @argc: The number of arguments.
@@ -29,15 +77,11 @@ int main(int argc, char *argv[])
     Elf64_Ehdr header;
 
     if (argc != 2)
-    {
         print_error("Usage: elf_header elf_filename");
-    }
 
     fd = open(argv[1], O_RDONLY);
     if (fd == -1)
-    {
         print_error("Error: Can't read file");
-    }
 
     read_bytes = read(fd, &header, sizeof(header));
     if (read_bytes == -1 || read_bytes != sizeof(header))
@@ -55,8 +99,7 @@ int main(int argc, char *argv[])
         print_error("Error: Not an ELF file");
     }
 
-    printf("ELF Header:\n");
-    // Print header information like Magic, Class, Data, etc.
+    print_elf_header(&header);
 
     close(fd);
     return (0);
