3f515cab62f249fc0d69dee3d9e63d6c2661caaf
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-27 18:21:19 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,15 @@ Defines a function to calculate the perimeter of an island.
 """
 
 def island_perimeter(grid):
+    """
+    Calculate the perimeter of an island.
+    
+    Args:
+        grid (list of list of int): The grid map of the island (1 for land, 0 for water).
+
+    Returns:
+        int: The perimeter of the island.
+    """
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
