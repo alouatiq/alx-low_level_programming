51fee47480e9ee28cf34aa436d839c734f9cba7d
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-28 22:40:47 +0100

    Create free_list.c

---
+++ 0x1E-search_algorithms/free_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * free_list - Frees a singly linked list.
+ * @list: Pointer to the head of the linked list.
+ */
+void free_list(listint_t *list)
+{
+    listint_t *temp;
+
+    while (list)
+    {
+        temp = list->next;
+        free(list);
+        list = temp;
+    }
+}
