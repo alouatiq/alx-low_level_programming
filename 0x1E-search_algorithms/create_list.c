57b075e904aa311341d31e8b10d1d3be8f013886
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-28 22:40:27 +0100

    Create create_list.c

---
+++ 0x1E-search_algorithms/create_list.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * create_list - Creates a singly linked list from an array of integers.
+ * @array: Pointer to the first element of the array.
+ * @size: Number of elements in the array.
+ *
+ * Return: Pointer to the head of the linked list.
+ */
+listint_t *create_list(int *array, size_t size)
+{
+    listint_t *head = NULL, *node = NULL, *prev = NULL;
+    size_t i;
+
+    for (i = 0; i < size; i++)
+    {
+        node = malloc(sizeof(listint_t));
+        if (!node)
+            return (NULL);
+        node->n = array[i];
+        node->index = i;
+        node->next = NULL;
+        if (!head)
+            head = node;
+        if (prev)
+            prev->next = node;
+        prev = node;
+    }
+
+    return (head);
+}
