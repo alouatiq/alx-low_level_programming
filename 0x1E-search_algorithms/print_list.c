05542d6a49fb5b9d35057b3032140553be8a575b
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-28 22:40:40 +0100

    Create print_list.c

---
+++ 0x1E-search_algorithms/print_list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * print_list - Prints a singly linked list.
+ * @list: Pointer to the head of the linked list.
+ */
+void print_list(const listint_t *list)
+{
+    while (list)
+    {
+        printf("Index[%lu] = [%d]\n", list->index, list->n);
+        list = list->next;
+    }
+}
