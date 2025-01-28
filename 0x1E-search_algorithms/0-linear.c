783e9f0212ce6d15214e96f73185b55717e41c85
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-28 22:28:49 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,15 +13,14 @@ int linear_search(int *array, size_t size, int value)
 {
     size_t i;
 
-    if (array == NULL)
+    if (!array)
         return (-1);
 
     for (i = 0; i < size; i++)
     {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
         if (array[i] == value)
-            return (i);
+            return ((int)i);
     }
-
     return (-1);
 }
