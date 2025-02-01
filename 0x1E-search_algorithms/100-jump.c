5f683aec162549cdc0bf22da804b62e9b3f4b368
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-01 09:08:09 +0100

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,39 +3,41 @@
 #include <stdio.h>
 
 /**
-	* jump_search - Searches for a value in a sorted array using Jump Search
-	* @array: Pointer to the first element of the sorted array
-	* @size: The number of elements in the array
-	* @value: The value to search for
-	*
-	* Return: The first index where value is located, or -1 if not found
-	*/
+ * jump_search - Searches for a value in a sorted array using Jump Search
+ * @array: Pointer to the first element of the sorted array
+ * @size: The number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The first index where value is located, or -1 if not found
+ */
 int jump_search(int *array, size_t size, int value)
 {
 	size_t step, prev = 0;
 
 	if (array == NULL || size == 0)
-	return (-1);
+		return (-1);
 
 	step = sqrt(size);
 
-	/* Jump in steps until the value is less than or equal to the current step */
+	/* Jump in steps until value is less than or equal to current step */
 	while (step < size && array[step] < value)
 	{
-	printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-	prev = step;
-	step += sqrt(size);
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		prev = step;
+		step += sqrt(size);
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
+	/* Corrected boundary print */
+	printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+	printf("Value found between indexes [%lu] and [%lu]\n", prev, (step < size) ? step : size - 1);
 
 	/* Perform linear search in the identified block */
 	while (prev < size && prev <= step)
 	{
-	printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-	if (array[prev] == value)
-	return ((int)prev);
-	prev++;
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return ((int)prev);
+		prev++;
 	}
 
 	return (-1);
